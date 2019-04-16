dc6c82bc6d4c98aa580d7001bb5b0580381adcf3
Laggage <1634205628@qq.com>
2019-04-16 12:39:15 +0800

    Submit

---
+++ Demo.DIYControl/ThemeManager/Accent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+
+namespace Demo.DIYControl.ThemeManager
+{
+    public class Accent
+    {
+        public ResourceDictionary Resources { get; set; }
+        public string Name { get; set; }
+
+        public Accent()
+        {
+
+        }
+
+        public Accent(string name,ResourceDictionary resources)
+        {
+            Name = name;
+            Resources = resources;
+        }
+
+        public Accent(string name,Uri resourcePath)
+        {
+            Name = name;
+            Resources = new ResourceDictionary()
+            {
+                Source = resourcePath
+            };
+        }
+    }
+}
